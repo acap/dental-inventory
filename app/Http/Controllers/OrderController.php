4872f5ab00c4e014bbc4b0febc8babe116285066
Rafizan Baharum <rafizan.baharum@canang.com.my>
2019-02-09 10:00:52 +0800

    Add print

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -8,7 +8,7 @@ use App\Http\Model\OrderItem;
 use App\Http\Model\StockCode;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Http\Request;
+use Konekt\PdfInvoice\InvoicePrinter;
 
 
 class OrderController extends Controller
@@ -118,4 +118,33 @@ class OrderController extends Controller
         return redirect("orders/detail/" . $orderNo);
     }
 
+    function print($orderNo){
+        $invoice = new InvoicePrinter();
+        /* Header settings */
+//        $invoice->setLogo("images/sample1.jpg");   //logo image path
+        $invoice->setColor("#007fff");      // pdf color scheme
+        $invoice->setType("Sale Invoice");    // Invoice Type
+        $invoice->setReference("INV-55033645");   // Reference
+        $invoice->setDate(date('M dS ,Y',time()));   //Billing Date
+        $invoice->setTime(date('h:i:s A',time()));   //Billing Time
+        $invoice->setDue(date('M dS ,Y',strtotime('+3 months')));    // Due Date
+        $invoice->setFrom(array("Seller Name","Sample Company Name","128 AA Juanita Ave","Glendora , CA 91740"));
+        $invoice->setTo(array("Purchaser Name","Sample Company Name","128 AA Juanita Ave","Glendora , CA 91740"));
+
+        $invoice->addItem("AMD Athlon X2DC-7450","2.4GHz/1GB/160GB/SMP-DVD/VB",6,0,580,0,3480);
+        $invoice->addItem("PDC-E5300","2.6GHz/1GB/320GB/SMP-DVD/FDD/VB",4,0,645,0,2580);
+        $invoice->addItem('LG 18.5" WLCD',"",10,0,230,0,2300);
+        $invoice->addItem("HP LaserJet 5200","",1,0,1100,0,1100);
+
+        $invoice->addTotal("Total",9460);
+        $invoice->addTotal("VAT 21%",1986.6);
+        $invoice->addTotal("Total due",11446.6,true);
+
+        $invoice->addBadge("Payment Paid");
+        $invoice->addTitle("Important Notice");
+        $invoice->addParagraph("No item will be replaced or refunded if you don't have the invoice with you.");
+        $invoice->setFooternote("My Company Name Here");
+        $output = fopen('php://output', 'w');
+        $invoice->render('example1.pdf','D');
+    }
 }
