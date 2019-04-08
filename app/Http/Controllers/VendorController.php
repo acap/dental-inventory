e9b0ee6e61bc9a8255355907610277205584e439
Asharaf Shukri <ashraf.shukri@terang.asia>
2019-04-08 20:08:28 +0800

    Create vendor

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -11,6 +11,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Model\StateCode;
 use App\Http\Model\Vendor;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class VendorController extends Controller
 {
@@ -49,13 +51,64 @@ class VendorController extends Controller
 
     function list()
     {
-      $vendorList = Vendor::all();
-      return view('/vendors/list')->with('vendorList',$vendorList);
+        $vendorList = Vendor::all();
+        return view('/vendors/list')->with('vendorList', $vendorList);
     }
 
     function detail($id)
     {
-       $detail = Vendor::where('ID','=',$id)->first();
-        return view('/vendors/detail')->with('detail',$detail);
+        $vendor = Vendor::where('ID', '=', $id)->first();
+        return view('/vendors/detail')
+            ->with('vendor', $vendor);
+    }
+
+    function edit($id)
+    {
+        $stateCodes = StateCode::all();
+        $vendor = Vendor::where('ID', '=', $id)->first();
+        return view('/vendors/edit')
+            ->with("vendor", $vendor)
+            ->with('stateCodes',$stateCodes);
+    }
+
+    function post_edit()
+    {
+
+        $id = $_POST['id'];
+        $company_name = $_POST['company_name'];
+        $contact_person_name = $_POST['contact_person_name'];
+        $address1 = $_POST['address1'];
+        $address2 = $_POST['address2'];
+        $address3 = $_POST['address3'];
+        $postcode = $_POST['postcode'];
+        $phone_no = $_POST['phone_no'];
+        $state_code_id = $_POST['state_code_id'];
+        info('company name'.$company_name);
+
+        try {
+            DB::connection()->beginTransaction();
+
+            $vendor = Vendor::where("ID", "=", $id)->first();
+            $vendor -> update([
+                'COMPANY_NAME'=>$company_name,
+                'CONTACT_PERSON_NAME'=>$contact_person_name,
+                'ADDRESS1'=>$address1,
+                'ADDRESS2'=>$address2,
+                'ADDRESS3'=>$address3,
+                'POSTCODE'=>$postcode,
+                'PHONE_NO'=>$phone_no,
+               'STATE_CODE_ID'=>$state_code_id
+            ]);
+
+            DB::connection()->commit();
+        } catch (\Exception $e) {
+            info($e);
+            DB::connection()->rollBack();
+            return redirect("errors/");
+        }
+
+        $stateCodes = StateCode::all();
+        return redirect('/vendors/detail/' . $vendor->ID)
+            ->with('xxx',$stateCodes);
     }
 }
